6710149a9a287f770637069b2882a0e129865cea
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2023-10-20 14:32:21 -0400

    debug and toLowerCase()

--- SweetLime-WebService/src/main/java/com/danayapie/SweetLimeWebService/utility/ExtractDomainName.java
+++ SweetLime-WebService/src/main/java/com/danayapie/SweetLimeWebService/utility/ExtractDomainName.java
@@ -1,17 +1,17 @@
 package com.danayapie.SweetLimeWebService.utility;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class ExtractDomainName {
 
     private static Logger logger = LoggerFactory.getLogger(ExtractDomainName.class);
 
     public static String getDomainName(String urlString) throws URISyntaxException {
+        logger.info("ExtractDomainName.getDomainName() invoked");
 
         if (!urlString.startsWith("http") && !urlString.startsWith("https")) {
             urlString = "http://" + urlString;
@@ -19,8 +19,9 @@ public class ExtractDomainName {
         URI uri = new URI(urlString);
         String host = uri.getHost();
         String domainName = host.startsWith("www.") ? host.substring(4) : host;
-        logger.info("domainName: " + domainName);
 
-        return domainName;
+        logger.debug("domainName: " + domainName);
+
+        return domainName.toLowerCase();
     }
 }
