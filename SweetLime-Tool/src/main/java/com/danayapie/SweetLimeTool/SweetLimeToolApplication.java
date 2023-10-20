9eafc64cc2866f3fd22385c4da94cec978659a56
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2023-10-20 19:52:37 -0400

    first commit

---
+++ SweetLime-Tool/src/main/java/com/danayapie/SweetLimeTool/SweetLimeToolApplication.java
@@ -0,0 +1,13 @@
+package com.danayapie.SweetLimeTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetLimeToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetLimeToolApplication.class, args);
+	}
+
+}
