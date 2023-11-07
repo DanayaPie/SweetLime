24c9f5921f41447f4cae4043e0429e66e7aa4139
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2023-11-06 23:20:57 -0500

    clean up

--- SweetLime-Tool/src/main/java/com/danayapie/SweetLimeTool/SweetLimeToolApplication.java
+++ SweetLime-Tool/src/main/java/com/danayapie/SweetLimeTool/SweetLimeToolApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Scanner;
 
 @SpringBootApplication
 public class SweetLimeToolApplication {
