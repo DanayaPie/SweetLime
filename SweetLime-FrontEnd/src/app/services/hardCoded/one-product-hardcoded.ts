07176fc40f193cb812cacbdb2ce60ad2c6807829
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2023-12-19 13:54:28 -0500

    update hardcoded product price dates

--- SweetLime-FrontEnd/src/app/services/hardCoded/one-product-hardcoded.ts
+++ SweetLime-FrontEnd/src/app/services/hardCoded/one-product-hardcoded.ts
@@ -2,24 +2,20 @@ import { Product } from "../../models/product";
 
 export const oneProductHardCoded: Product[] = [
   {
-    productId: 'c657fbc2-20b4-42fb-8b60-2d051b4bed2e',
-    productName: 'Haruharu WONDER - Black Rice Hyaluronic Toner - Fragrance Free - 300ml',
-    productUrl: 'https://www.stylevana.com/en_US/haruharu-wonder-black-rice-hyaluronic-toner-fragrance-free-300ml.html',
-    createdDate: '1/20/1970',
-    deletedDate: '',
-    imagePath: 'https://cdn.stylevana.com/media/catalog/product/cache/1/image/1000x1231/17f82f742ffe127f42dca9de82fb58b1/h/a/haruharu-wonder-black-rice-hyaluronic-toner-fragrance-free-300ml-899.jpg',
+    productId: "8a218519-53a0-4115-a5fa-6a962b56a047",
+    productName: "numbuzin - No.3 Super Glowing Essence Toner - 200ml",
+    productUrl: "https://www.stylevana.com/en_US/numbuz-n-no-3-super-glowing-essence-toner-200ml.html",
+    createdDate: "10/25/2023",
+    deletedDate: "",
+    imagePath: "https://cdn.stylevana.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/u/numbuzin-no-3-super-glowing-essence-toner-200ml-529.jpg",
     options: {
-      Size: '300ml',
-      Color: 'No Color',
+      Size: "200ml",
+      Color: "NOC",
     },
     priceHistory: [
-      { Price: 22.4, UpdatedDate: '1/20/1970' },
-      { Price: 14.39, UpdatedDate: '1/20/1970' },
-      { Price: 19.2, UpdatedDate: '1/20/1970' },
-      { Price: 19.19, UpdatedDate: '1/20/1970' },
-      { Price: 17.19, UpdatedDate: '1/20/1970' },
-      { Price: 22.39, UpdatedDate: '1/20/1970' },
+      { Price: 16.1, UpdatedDate: "10/25/2023" },
+      { Price: 14.09, UpdatedDate: "12/15/2023" },
     ],
-    newestPrice: 22.4,
+    newestPrice: 14.09,
   }
 ];
