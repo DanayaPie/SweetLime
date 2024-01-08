82f62a874c5b575001ab46e98de2f6f1dffa454c
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2024-01-08 15:02:01 -0500

    routing module with supported-web as home page

--- SweetLime-FrontEnd/src/app/app-routing.module.ts
+++ SweetLime-FrontEnd/src/app/app-routing.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProductContainerComponent } from './components/product-container/product-container.component';
-import { ProductComponent } from './components/product-container/product/product.component';
-import { HeaderComponent } from './components/header/header.component';
 import { ProductListComponent } from './components/product-container/product-list/product-list.component';
+import { SupportedWebComponent } from './components/supported-web/supported-web.component';
 
 const routes: Routes = [
-    { path: 'home', component: HeaderComponent },
+    { path: '', redirectTo: '/supported-web', pathMatch: 'full' }, // Redirect to supported-web or any default path
     { path: 'product/:id', component: ProductContainerComponent },
     { path: 'product-list', component: ProductListComponent },
-    { path: '**', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'supported-web', component: SupportedWebComponent },
+    { path: '**', redirectTo: '/supported-web', pathMatch: 'full' }, // Redirect to supported-web for unknown paths
 ];
+
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
